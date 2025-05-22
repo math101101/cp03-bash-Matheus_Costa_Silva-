0eba8537c5b3aa019959131893fd2d29cc31d63e
math101101 <rm561598@fiap.com.br>
2025-05-22 09:22:01 -0300

    argumentos colocados no minfo.sh
    adicionei os argumentos necessários

--- minfo.sh
+++ minfo.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-echo "hello world"
+
+# Verifica se um argumento foi fornecido
+if [ $# -ne 1 ]; then
+    echo "Uso: $0 {hostname|uptime|disk|all}"
+    exit 1
+fi
+
+case "$1" in
+    hostname)
+        hostname
+        ;;
+    uptime)
+        uptime -p
+        ;;
+    disk)
+        df -h /
+        ;;
+    all)
+        hostname
+        uptime -p
+        df -h /
+        ;;
+    *)
+        echo "Opção inválida: $1"
+        echo "Uso: $0 {hostname|uptime|disk|all}"
+        exit 1
+        ;;
+esac
